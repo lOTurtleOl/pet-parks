bed09941731e8bc1f6ddb528af73b723e6aec7d6
Andrew Hughes <andyashmore1@gmailcom>
2025-01-23 09:40:18 -0600

    complete contributor operation, must fix error 500

--- src/main/java/pet/park/controller/model/ContributorData.java
+++ src/main/java/pet/park/controller/model/ContributorData.java
@@ -7,20 +7,38 @@ import java.util.HashSet;
 import java.util.Set;
 
 import lombok.Data;
-import lombok.Value;
+import lombok.NoArgsConstructor;
+import pet.park.entity.Amenity;
+import pet.park.entity.Contributor;
 import pet.park.entity.GeoLocation;
+import pet.park.entity.PetPark;
 
 /**
  * 
  */
 @Data
 public class ContributorData {
-	private Long contributorId;   // looks like contributor_id
+	/**
+	 * @param save
+	 */
+
+	private Long contributorId; // looks like contributor_id
 	private String contributorName;
 	private String contributorEmail;
 	private Set<PetParkResponse> petParks = new HashSet<>();
-	
-	@Value
+
+	public ContributorData(Contributor contributor) {
+		contributorId = contributor.getContributorId();
+		contributorName = contributor.getContributorName();
+		contributorEmail = contributor.getContributorEmail();
+
+		for (PetPark petPark : contributor.getPetParks()) {
+			petParks.add(new PetParkResponse(petPark));
+		}
+	}
+
+	@Data
+	@NoArgsConstructor
 	static class PetParkResponse {
 		private Long petParkId;
 		private String parkName;
@@ -29,5 +47,18 @@ public class ContributorData {
 		private String country;
 		private GeoLocation geoLocation;
 		private Set<String> amenities = new HashSet<>();
+
+		PetParkResponse(PetPark petPark) {
+			petParkId = petPark.getPetParkId();
+			parkName = petPark.getParkName();
+			directions = petPark.getDirections();
+			stateOrProvince = petPark.getStateOrProvince();
+			country = petPark.getCountry();
+			geoLocation = new GeoLocation(petPark.getGeoLocation());
+
+			for (Amenity amenity : petPark.getAmenities()) {
+				amenities.add(amenity.getAmenity());
+			}
+		}
 	}
 }
